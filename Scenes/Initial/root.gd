f2f4b48d6cf69b5b0202ff19abdbdac31446c6fa
JinxedGrim <jinxedgrimyt@gmail.com>
2026-01-31 20:34:18 -0500

    Added colliders to generated tiles

--- Scenes/Initial/root.gd
+++ Scenes/Initial/root.gd
@@ -1,8 +1,4 @@
 extends Node2D
-#this script  should contain some game logic and some 
-
-# this will run once everything else is loaded im gonna do terrain generation here 
-# but ideally we use await / maybe generate from another small scene (display a loading splash
 
 # these are all in game units
 const LevelWidth = 50
@@ -11,21 +7,36 @@ const TileScale = 5.0
 
 var TileTexture: Texture2D
 
-func _GenerateTiles() -> void:
+# this will run once everything else is loaded im gonna do terrain generation here 
+# but ideally we use await / maybe generate from another small scene (display a loading splash
+func _generateTiles() -> void:
 	# original texture size in pixels
-	var tex_size = TileTexture.get_size()
+	var textureSize = TileTexture.get_size()
 	
 	for x in range(LevelWidth):
 		for y in range(LevelHeight):
-			var block = Sprite2D.new()
-			block.texture = TileTexture
-			block.position = Vector2(x * TileScale, y * TileScale)
-			block.scale = Vector2(TileScale / tex_size.x, TileScale / tex_size.y)
-			add_child(block)
+			var tile = StaticBody2D.new()
+			
+			var tileSprite = Sprite2D.new()
+			tileSprite.texture = TileTexture
+			tileSprite.position = Vector2(0, 0)
+			tileSprite.scale = Vector2(TileScale / textureSize.x, TileScale / textureSize.y)
+			tile.add_child(tileSprite)
+			
+			var collider = CollisionShape2D.new()
+			var shape = RectangleShape2D.new()
+			shape.extents = (textureSize * TileScale) / 2  # half-width/height
+			collider.shape = shape
+			collider.position = Vector2(0, 0)
+			tile.add_child(collider)
+
+			tile.position = Vector2(x * TileScale, y * TileScale)
+			
+			add_child(tile)
 
 func _ready() -> void:
-	TileTexture = load("res://Assets/Tiles/Tile.png")
-	_GenerateTiles()
+	TileTexture = load("res://Assets/Tiles/Tile_2.png")
+	_generateTiles()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

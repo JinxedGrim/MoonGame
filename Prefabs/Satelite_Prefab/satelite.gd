37254aab701f737a019d402511bdba3847c6a909
Griffin Smith <smitgm03@pfw.edu>
2026-02-03 06:34:39 -0500

    Slightly more cleanup

--- Prefabs/Satelite_Prefab/satelite.gd
+++ Prefabs/Satelite_Prefab/satelite.gd
@@ -4,8 +4,3 @@ var SatSprite = null
 func _ready() -> void:
 	SatSprite = $StaticBody2D/SateliteSprite
 	SatSprite.play("Idle")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
